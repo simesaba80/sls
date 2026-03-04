98f15baccbb6b9772d946b16c068c945cb06777e
simesaba80 <simesaba1120@gmail.com>
2026-03-04 12:46:49 +0900

    ファイルとディレクトリ名の取得機能作成

--- Sources/sls/sls.swift
+++ Sources/sls/sls.swift
@@ -1,14 +1,26 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+import Foundation
 import ArgumentParser
 
 @main
 struct sls: ParsableCommand {
-    @Argument(help: "名前を入力")
-    var name: String
+    @Flag(name: .shortAndLong, help: "全て表示")
+    var all: Bool = false
     
+    @Option(name: .shortAndLong, help: "虚無オプション")
+    var name: Int?
+    @Argument(help: "パスを入力")
+    var path: String
+        
     mutating func run() throws {
-        print("Hello, \(name)!")
+        print("Hello \(path)")
+        var fileGeter = FileGeter()
+        let files = fileGeter.getFile(path: path, all: all)
+        for file in files {
+            print(file, terminator: " ")
+        }
+        print()
     }
 }
